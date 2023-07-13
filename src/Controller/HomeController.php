ab0879a11262c5a884ff5736ac928f5368884352
itopi-fr <88286107+itopi-fr@users.noreply.github.com>
2023-07-13 16:33:27 +0200

    temp commit for session

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\File;
+use App\Entity\Trick;
+use App\Entity\TrickCategory;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,10 +13,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
 //    #[Route(['/', '/home'], name: 'app_home')]
-    #[Route(['/{pseudo}'], name: 'app_home')]
-    public function index(User $user): Response
+//    #[Route(['/{pseudo}'], name: 'app_home')]
+    #[Route(['/{slug}'], name: 'app_home')]
+    public function index(Trick $obj): Response
     {
-        dump($user->getForumMessages());
+        dump($obj);
+
+        // Test Trick
+        // URL : /
+        dump($obj->getTitle());
+        dump($obj->getCategory()->getTitle());
+
+        // User
+//        dump($obj->getAvatarFile());
+//        dump($obj->getForumMessages());
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
